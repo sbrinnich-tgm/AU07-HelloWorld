b856664c6c59c77177d42f52950b1943ddf343ec
Brinnich Selina <sbrinnich@student.tgm.ac.at>
2014-02-22 16:26:52 +0100

    Main-Klasse erstellt
    Methods-Klasse erstellt
    	1. Methode (singleOutput()) erstellt

---
+++ sbrinnich/Main.java
@@ -0,0 +1,9 @@
+package sbrinnich;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ sbrinnich/Methods.java
@@ -0,0 +1,9 @@
+package sbrinnich;
+
+public class Methods {
+
+	public static void singleOutput(){
+		System.out.println("Hello World!");
+	}
+	
+}
