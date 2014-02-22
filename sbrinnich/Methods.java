c3ef999740224d81229879170186c25f5227e8c4
Brinnich Selina <sbrinnich@student.tgm.ac.at>
2014-02-22 16:31:55 +0100

    Methods-Klasse bearbeitet
    	3 neue Methoden hinzugefügt

--- sbrinnich/Methods.java
+++ sbrinnich/Methods.java
@@ -6,4 +6,20 @@ public class Methods {
 		System.out.println("Hello World!");
 	}
 	
+	public static void nOutputs(int n){
+		for(int i = 0; i < n; i++){
+			System.out.println("Hello World!");
+		}
+	}
+	
+	public static void singleOutput(String txt){
+		System.out.println(txt);
+	}
+	
+	public static void nOutputs(int n, String txt){
+		for(int i = 0; i < n; i++){
+			System.out.println(txt);
+		}
+	}
+	
 }
